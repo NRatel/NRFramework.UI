9f9d4903294f3275a45c108871b2b651757d7de3
聂红强 <36918549+NRatel@users.noreply.github.com>
2022-10-30 00:04:39 +0800

    Update UIEditorUtility.cs

--- Assets/Scripts/NRFramework/Editor/UI/UIEditorUtility.cs
+++ Assets/Scripts/NRFramework/Editor/UI/UIEditorUtility.cs
@@ -8,16 +8,16 @@ namespace NRFramework
     {
         static public Texture GetIconByType(Type type)
         {
-            //系统图标
-            Texture icon = EditorGUIUtility.ObjectContent(null, type).image;
+            //系统内置图标
+            Texture systemIcon = EditorGUIUtility.ObjectContent(null, type).image;
 
-            //自定义组件图标
-            //todo
+            //自定义组件图标   //todo自行添加
+            Texture customIcon = null;
 
             //默认图标
             Texture csScriptIcon = EditorGUIUtility.IconContent("cs Script Icon").image;
 
-            return icon ?? csScriptIcon;
+            return systemIcon ?? customIcon ?? csScriptIcon;
         }
     }
 }
