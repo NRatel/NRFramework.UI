5dcd0488a3d07b2fa96ca1ec017971fecc39cfd5
聂红强 <36918549+NRatel@users.noreply.github.com>
2022-11-14 21:05:15 +0800

    Update NRFrameworkEditorSetting.cs

--- Assets/Scripts/NRFramework/Editor/NRFrameworkEditorSetting.cs
+++ Assets/Scripts/NRFramework/Editor/NRFrameworkEditorSetting.cs
@@ -6,20 +6,10 @@ namespace NRFramework
     //路径相对 Application.dataPath
     public class NRFrameworkEditorSetting : ScriptableObject
     {
-        //Asset
-        //public enum AssetUsageMode
-        //{
-        //    Editor = 1,
-        //    Local = 2,
-        //    Remote = 3,
-        //}
-        //public AssetUsageMode assetUsageMode = AssetUsageMode.Editor;       //编辑器下运行时
-        //public string assetBundleRemoteUrl = "https://127.0.0.1";
-
-        // UI
         public bool enableOpElementHierarchy = true;
 
         public string uiPrefabRootDir = "GameRes/GUI/Prefabs";
+
         // UI类生成根目录（相对于 Application.dataPath）
         // 将在相对路径下创建对应基类。
         // 将在相对路径下创建快捷类。创建后应自行改名（避免覆盖）。
